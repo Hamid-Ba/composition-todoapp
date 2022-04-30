9ba465be4d09e9132f2c2a21e723dac3a2ea4fa6
hamidba <balalzadehhamid79@gmail.com>
2022-05-01 00:58:50 +0430

    Complete Drag And Drop

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,12 @@
     <TodoAdd @ItemAdded="AddTodo"/>
     <ul class="todos">
 
-      <TodoItem v-for="(item,index) in todoList" :key="index" :todo="item"/>
+      <TodoItem v-for="(item,index) in todoList" 
+      :key="index" 
+      :todo="item"
+      @dragover.prevent
+      @dragstart="DragStarted(index)"
+      @drop="DropStarted(index)"/>
       
     </ul>
     <div class="card stat">
@@ -27,14 +32,25 @@ import Header from './components/AppHeader.vue'
 import TodoAdd from './components/TodoAdd.vue'
 import TodoItem from './components/TodoItem.vue'
 import AppFooter from './components/AppFooter.vue'
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 
 var todoList = reactive([])
+var dragIndex = ref(-1)
 
-function AddTodo(todoTitle){
+let AddTodo = (todoTitle) => {
   const todoId = Math.random().toString(15).slice(3);
   var todo = {id:todoId,title : todoTitle,isCompleted : false}
   todoList.push(todo)
 }
+
+let DragStarted = (index) => {
+  dragIndex.value = index;
+}
+
+let DropStarted = (index) => {
+  var item = todoList.splice(dragIndex.value,1)[0];
+  todoList.splice(index,0,item);
+}
+
 
 </script>
