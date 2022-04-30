57342e9265b7345abc7994fa6b8e7cb1cd941ea2
hamidba <balalzadehhamid79@gmail.com>
2022-04-30 21:33:20 +0430

    Complete AppHeader And Change Theme

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,18 +3,27 @@
 
      <h1>لیست وظایف </h1>
     
-     <button id="theme-switcher">
-         <img src="../assets/icon-sun.svg" alt="Change color theme" />
+     <button id="theme-switcher" @click="changeTheme">
+         <img :src="currentIcon" alt="Change color theme" />
      </button>
  </header>
 </template>
 
-<script>
-export default {
+<script setup>
+import {ref} from "vue";
+import SunIcon from '../assets/icon-sun.svg'
+import MoonIcon from '../assets/icon-moon.svg'
 
+var currentIcon = ref(SunIcon) 
+
+function changeTheme(){
+     if(currentIcon.value == SunIcon){
+         currentIcon.value = MoonIcon
+         document.querySelector('body').classList.add('light');
+     }
+     else{
+         currentIcon.value = SunIcon
+         document.querySelector('body').classList.remove('light');
+     } 
 }
 </script>
-
-<style>
-
-</style>
